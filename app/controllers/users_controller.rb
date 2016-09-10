2375037bc6b71c25ae4332ca2b86ea98f0472bb2
WDIDavidKim <davidjkim1984@gmail.com>
2016-09-09 23:13:12 -0700

    Created user new page with partial.  Need uploadcare setup for photos.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    set_user
+    if @user.save
+      login(@user)
+      flash[:notice] = "User account created! Welcome to NapZZZ"
+      redirect_to @user
+    else
+      flash[:notice] = "There was a problem creating your account. #{@user.errors.full_messages.join(', ')}."
+      redirect_to new_user_path
+    end
+  end
 end
