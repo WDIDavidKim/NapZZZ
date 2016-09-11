bb01c219268a28ecd696312e909c80f2d25f0671
WDIDavidKim <davidjkim1984@gmail.com>
2016-09-11 15:25:49 -0700

    Listings controller with update and edit

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -16,11 +16,30 @@ class ListingsController < ApplicationController
     @listing = Listing.new(listing_params)
     @listing.user_id = session[:user_id]
     if @listing.save
-      flash[:success] = "Your listing has been posted"
+      flash[:success] = "Your listing \"#{@listing.title}\" has been posted"
       redirect_to listing_path(@listing)
     else
       flash[:notice] = "Your listing could not be created: #{@listing.errors.full_messages.join(', ')}."
       redirect_to new_listing_path
+    end
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+    auth_fail("edit other user's listing!", @listing) if !auth_route(@listing.user)
+  end
+
+  def update
+    set_article
+    if auth_route(@listing.user)
+      if @listing.update(listing_params)
+        flash[:success] = "#{@listing.title} was updated!"
+        redirect_to listing_path
+      else
+        render :edit
+      end
+    else
+      auth_fail("update other user's listing!", listing_path)
     end
   end
 end
