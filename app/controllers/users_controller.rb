6726316303dc8c68182f9358cc56019b6bb2fa71
WDIDavidKim <davidjkim1984@gmail.com>
2016-09-11 02:19:54 -0700

    new user can now be created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,20 @@ class UsersController < ApplicationController
     if @user.save
       login(@user)
       flash[:notice] = "User account created! Welcome to NapZZZ"
-      redirect_to @user
+      redirect_to "/"
     else
       flash[:notice] = "There was a problem creating your account. #{@user.errors.full_messages.join(', ')}."
       redirect_to new_user_path
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest, :profile_photo)
+  end
+
+  def set_user
+    @user = User.new(user_params)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   root to: "listings#index"
 
   get "/users/new", to: "users#new", as: "new_user"
@@ -8,5 +8,13 @@ Rails.application.routes.draw do
   get "/users/:id/edit", to: "users#edit", as: "edit_user"
   patch "/users/:id", to: "users#update"
   delete "/users/:id", to: "users#destroy", as: "destroy_user"
+
+  get "/login", to: "sessions#new", as: "new_session"
+  get "/logout", to: "sessions#destroy"
+  post "/sessions", to: "sessions#create", as: "create_session" 
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
