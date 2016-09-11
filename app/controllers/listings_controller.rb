e003c1d9da770acc0069e903597b1e3be6e5e077
WDIDavidKim <davidjkim1984@gmail.com>
2016-09-11 12:50:14 -0700

    listings controller setup index show and new

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,4 +3,12 @@ class ListingsController < ApplicationController
   def index
     @listings = Listing.all
   end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def new
+    @listing = Listing.new
+  end
 end
