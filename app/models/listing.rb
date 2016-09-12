603f84b3271a75d30b6b5e25c9866bbb05b16d8b
WDIDavidKim <davidjkim1984@gmail.com>
2016-09-11 20:13:07 -0700

    Listings and user model validations with foreign keys

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,6 @@
 class Listing < ApplicationRecord
+  validates :title, length: {in: 2..75}, presence: true
+  validates :content, length: {in: 2..2000}, presence: true
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, uniqueness: true, length: { in: 2..150}, presence: true
+  validates :first_name, length: { in: 2..50}, presence: true
+  validates :last_name, length: { in: 2..75}, presence: true
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_many :listings
 end
