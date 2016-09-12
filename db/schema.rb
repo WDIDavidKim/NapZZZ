a210b070e8679b3547e4ea659fbb3c35d68f5c40
WDIDavidKim <davidjkim1984@gmail.com>
2016-09-11 21:40:15 -0700

    Ready for rollback and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911025202) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "listings", force: :cascade do |t|
-    t.string   "title"
-    t.string   "content"
-    t.string   "image"
-    t.string   "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "profile_photo"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
 
 end
